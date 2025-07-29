5974aee911d348bea1134d7f8ee1b3e16f9ea485
Yasser Abdel-Maksoud <yabdelmaksoud.c@tahakom.com>
2025-07-30 01:32:17 +0300

    Adds navbar component for site navigation
    Adds a navigation bar component with links to different
    sections of the page, improving user experience.
    
    The navbar utilizes a configuration file for link definitions
    and provides smooth scrolling functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@components/header/header.component';
+import { NavbarComponent } from '@components/navbar/navbar.component';
 
 @Component({
   selector: 'sred-ts-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [HeaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/constants/navbar-links.constant.ts
@@ -0,0 +1,8 @@
+import { NavbarLink } from "@models/navbar-links.model";
+
+export const NAV_LINKS: NavbarLink[] = [
+    { label: 'Employee Grid', anchor: 'employee-grid' },
+    { label: 'Project Breakdown', anchor: 'project-breakdown' },
+    { label: 'Project Summary', anchor: 'project-summary' },
+    { label: 'Year Projection', anchor: 'year-projection' }
+];
