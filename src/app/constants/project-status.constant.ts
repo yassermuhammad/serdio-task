b16bdd52666068ab30623f014dd8a1a97cac96ce
Yasser Abdel-Maksoud <yabdelmaksoud.c@tahakom.com>
2025-07-30 05:17:13 +0300

    Fix GitHub Pages deployment permissions

---
+++ src/app/constants/project-status.constant.ts
@@ -0,0 +1,14 @@
+export const PROJECT_STATUS = {
+    COMPLETED: {
+        label: 'Completed',
+        color: 'green'
+    },
+    IN_PROGRESS: {
+        label: 'In Progress',
+        color: 'yellow'
+    },
+    PLANNING: {
+        label: 'Planning',
+        color: 'red'
+    }
+}
